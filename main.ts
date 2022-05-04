c944cb3c4d74bd23beb2d2959e5dd5234506cd2f
ZL189 <104779277+ZL189@users.noreply.github.com>
2022-05-04 10:28:13 +0800

    1.0版本

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+startbit.startbit_Init()
+basic.showIcon(IconNames.Asleep)
+basic.forever(function () {
+    if (startbit.startbit_line_followers(startbit.startbit_LineFollowerSensors.S2, startbit.startbit_LineColor.Black)) {
+        startbit.startbit_setMotorSpeed(100, 70)
+    } else if (startbit.startbit_line_followers(startbit.startbit_LineFollowerSensors.S3, startbit.startbit_LineColor.Black)) {
+        startbit.startbit_setMotorSpeed(70, 100)
+    } else if (startbit.startbit_readLineFollowerStatus(startbit.startbit_lineFollowPort.port1, startbit.startbit_lineFollower.S1_IN_S2_IN)) {
+        startbit.startbit_setMotorSpeed(0, 0)
+    } else {
+        startbit.startbit_setMotorSpeed(70, 70)
+    }
+})
